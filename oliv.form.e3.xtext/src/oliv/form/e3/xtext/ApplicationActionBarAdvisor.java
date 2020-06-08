6108ac6230fc1bbb367a963fe1504671b6f390fa
Olivier Germon <olivier.germon@gmail.com>
2020-06-08 09:42:34 +0200

    template + fix

--- oliv.form.e3.xtext/src/oliv/form/e3/xtext/ApplicationActionBarAdvisor.java
+++ oliv.form.e3.xtext/src/oliv/form/e3/xtext/ApplicationActionBarAdvisor.java
@@ -2,11 +2,16 @@ package oliv.form.e3.xtext;
 
 import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	private IWorkbenchAction preferencesAction;
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
@@ -14,13 +19,22 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	
 	@Override
 	protected void makeActions(IWorkbenchWindow window) {
-		super.makeActions(window);
+	super.makeActions(window);
+	{
+	preferencesAction = ActionFactory.PREFERENCES.create(window);
+	register(preferencesAction);
 	}
+	} 
 	
 	@Override
 	protected void fillMenuBar(IMenuManager menuBar) {
-		super.fillMenuBar(menuBar);
-	}
+	super.fillMenuBar(menuBar);
+
+	MenuManager menuManager = new MenuManager("New MenuManager");
+	menuManager.setMenuText("window");
+	menuBar.add(menuManager);
+	menuManager.add(preferencesAction);
+	} 
 	
 	@Override
 	protected void fillCoolBar(ICoolBarManager coolBar) {
